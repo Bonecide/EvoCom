ad76710df966096dd1f1e2c562dceec7b605e1c4
Kasymov Marsel <marselkasymov23@gmail.com>
2022-05-11 19:47:04 +0600

    fixed some bugs on header

--- src/Section/AboutUs/AboutUs.jsx
+++ src/Section/AboutUs/AboutUs.jsx
@@ -3,7 +3,7 @@ export default function AboutUs () {
 
     return(
 
-        <div className='AboutUs_container'>
+        <div className='AboutUs_container' id='AboutUs'>
             <div className='container'>
                 <h2>
                     sdfasdf
