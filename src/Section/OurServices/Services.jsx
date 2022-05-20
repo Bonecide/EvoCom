282fbf1a8744b324798265c5e37e5613a40f6a8a
Vladislav <vsamukhin@gmail.com>
2022-05-20 22:57:14 +0600

    services

--- src/Section/OurServices/Services.jsx
+++ src/Section/OurServices/Services.jsx
@@ -2,23 +2,23 @@ import '../OurServices/Services.scss'
 
 export default function Services(){
   return(
-    <div className="services">
+    <div className="services" id='Services'>
       <div className="container">
         <h1 className="title">Наши сервисы</h1>
         <div className="description">
           <div className="services_section">
-            <h2 className="services_title">Монтаж оптических<br/> и медных кабелей</h2>
+            <h2 className="services_title">Монтаж оптическихи медных кабелей</h2>
             <p className="services_description">
-               Наши специалисты осуществляют прокладку<br/> 
-               и сварку кабелей волоконно-оптических систем <br/>
-               связи, а также классических медных кабелей <br/> 
+               Наши специалисты осуществляют прокладку
+               и сварку кабелей волоконно-оптических систем 
+               связи, а также классических медных кабелей  
                связи и передачи данных.
             </p>
           </div>
           <div className="services_section">
-            <h2 className="services_title">Монтаж структурированных <br/>кабельных систем </h2>
+            <h2 className="services_title">Монтаж структурированных кабельных систем </h2>
             <p  className="services_description">
-               Мы производим профессиональный качественный<br/> 
+               Мы производим профессиональный качественный
                монтаж структурированных кабельных систем.
             </p>
           </div>
