c53304995fbc3ed1f03a1f704fcc78d4f5b74890
Vladislav <vsamukhin@gmail.com>
2022-06-03 17:13:03 +0600

    ChooseUs , Services

--- src/Section/ChooseUs/ChooseUs.jsx
+++ src/Section/ChooseUs/ChooseUs.jsx
@@ -1,48 +1,55 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react'
 import '../ChooseUs/ChooseUs.scss'
 
 export default function ChooseUs (){
 
+  const [data , setData] = useState();
+
+  const fetchData = () => {
+    axios.get('http://167.99.35.71/choose_us/')
+    .then(res => {
+      setData(res.data)
+    })
+  } 
+
+  useEffect(() => {
+    fetchData()
+  },[])
+
   return (
     <div className="chooseUs ">
-      <div className='chooseUs_block container'>
-        <h1 className="chooseUs_title">Почему выбирают нас</h1> 
-      </div>  
-     <div className='block_choose container'>
-      <div className="block">
-          <div className='block_text'>
-            <h3 className='block_title'>Мы поставляем</h3>
-            <p className='block_description'>
-              Оборудование известных мировых 
-              телеком компаний как: Eltek, 
-              DeltaSolution, NFWARE, Siklu,
-              RadWin, Cambium, Ceragon,
-              Старлинк и других.
-            </p>
-          </div>
-          <span className='span'></span>
-          <div className='block_text'>
-            <h3 className='block_title'>Мы осуществляем</h3>
-            <p className='block_description'>
-            Реализации проектов полного цикла
-            от подключения удаленных
-            объектов до построения полномасштабных 
-            инфокоммуникационных сетей
-            корпоративного уровня.
-            </p>
-          </div>
-          <span className='span'></span>
-          <div className='block_text'>
-            <h3 className='block_title'>Мы предлагаем</h3>
-            <p className='block_description'>
-            Широкий спектр услуг в области 
-            IT логистики с использованием
-            оптимальной логистической
-            схемы по доставке и хранению
-            товара до склада заказчика.
-            </p>
+    {data?
+     <div>
+        <div className='chooseUs_block container'>
+          <h1 className="chooseUs_title">Почему выбирают нас</h1> 
+        </div>  
+      <div className='block_choose container'>
+        <div className="block">
+            <div className='block_text'>
+              <h3 className='block_title'> {data[1].title}</h3>
+              <p className='block_description'>
+                {data[1].text}
+              </p>
+            </div>
+            <span className='span'></span>
+            <div className='block_text'>
+              <h3 className='block_title'>{data[0].title}</h3>
+              <p className='block_description'>
+                {data[0].text}
+              </p>
+            </div>
+            <span className='span'></span>
+            <div className='block_text'>
+              <h3 className='block_title'>{data[2].title}</h3>
+              <p className='block_description'>
+                {data[2].text}
+              </p>
+            </div>
           </div>
         </div>
      </div> 
+     : 'Loading ...' }
     </div>
   )
 }
--- src/Section/OurServices/Services.jsx
+++ src/Section/OurServices/Services.jsx
@@ -1,29 +1,43 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react'
 import '../OurServices/Services.scss'
 
 export default function Services(){
+
+  const [data , setData] = useState();
+
+  const fetchData =()=>{
+    axios.get('http://167.99.35.71/our_services/')
+    .then(res => {
+      setData(res.data)
+    })
+  }
+
+  useEffect(()=>{
+    fetchData()
+  },[])
+
   return(
     <div className="services" id='Services'>
+      {data?
       <div className="container">
         <h1 className="title">Наши сервисы</h1>
         <div className="description">
           <div className="services_section">
-            <h2 className="services_title">Монтаж оптическихи <br/> медных кабелей</h2>
+            <h2 className="services_title">{data[0].title}</h2>
             <p className="services_description">
-               Наши специалисты осуществляют прокладку
-               и сварку кабелей волоконно-оптических систем 
-               связи, а также классических медных кабелей  
-               связи и передачи данных.
+              {data[0].text}
             </p>
           </div>
           <div className="services_section">
-            <h2 className="services_title">Монтаж структурированных<br/> кабельных систем </h2>
+            <h2 className="services_title">{data[1].title}</h2>
             <p  className="services_description">
-               Мы производим профессиональный качественный
-               монтаж структурированных кабельных систем.
+              {data[1].text}
             </p>
           </div>
         </div>
       </div>
+      : 'Loading...'}
     </div>
   )
 }
