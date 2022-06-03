e0f62feced5ffb9c101caafb7ec72535c3d2ed28
Kasymov Marsel <marselkasymov23@gmail.com>
2022-06-03 13:37:00 +0600

    Подключил бэк

--- src/Section/Amenits/Amenits.jsx
+++ src/Section/Amenits/Amenits.jsx
@@ -1,8 +1,21 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 import './Amenits.scss'
 export default function Amenits() {
 
+    const [data,setData] = useState()
+    const fetchData = () => {
+       axios.get('http://167.99.35.71/services/')
+    .then(res =>{
+      setData(res.data)
+    })}
+    useEffect (()=> {
+      fetchData()
+    },[])
     return (
+    
         <div id="Amenit">
+            {data ? 
             <div className="container">
                 <h2 className='Amenits_headline'>Наши услуги</h2>
                 <div className="Amenits" >
@@ -10,40 +23,37 @@ export default function Amenits() {
                     <div className="Amenits_first_row">
                       <div className='Amenits_borders_first'>
                         <div className='Amenits_first_row_first_block'>
-                                <h3>Дистрибуция продуктов и решений ведущих производителей телекоммуникационной отрасли.</h3>
-                                <p>Мы являемся официальными представителем известных мировых телекоммуникационных компаний как: Extreme, DeltaSolution, NFWARE, Siklu, RadWin, Cambium, Ceragon. </p>
+                                <h3>{data[2].title}</h3>
+                                <p>{data[2].text}</p>
                         </div>
                       </div> 
                      <div className='Amenits_borders'>
                         <div className='Amenits_first_row_second_block'>
-                                <h3>Выстроенная система логистики. Поставка оборудования.</h3>
-                                <p>Компания «Эвоком Сервис», осуществляет продажу и доставку оборудования со склада производителей/партнеров в любую точку стран СНГ до склада заказчика в оптимальные сроки. </p>
+                                <h3>{data[0].title}</h3>
+                                <p>{data[0].text}</p>
                         </div>
                      </div>     
                     </div>
                     <div className="Amenits_second_row">
                        <div className='Amenits_borders_first'>
                         <div className='Amenits_second_row_first_block'>
-                                <h3>Экспертиза и Консалтинг.</h3>
-                                <p>Опыт реализованных проектов и партнерские статусы у ведущих производителей оборудования позволяют компании «Эвоком Сервис» подбирать новейшие инновационные решения для оптимизации бизнеса заказчика. 
-                                    <br /> 
-                                Наши высококвалифицированные специалисты проводят аудит ИТ-инфраструктуры заказчика 
-                                и предлагают рекомендации на основе постоянного анализа тенденций телекоммуникационного рынка.
+                                <h3>{data[3].title}</h3>
+                                <p>{data[3].text}
                                 </p>
                         </div>
                         </div>
                         <div className='Amenits_borders'>
                         <div className='Amenits_second_row_second_block'>
-                                <h3>Комплекс услуг по сопровождению проектов.</h3>
-                                <p>Компания имеет богатый опыт в реализации 
-                                и интеграции технологически сложных проектов, в том числе в территориально
-                                распределенных структурах. Все работы проводятся в сжатые сроки, 
-                                в удобном для заказчика режиме.</p>
+                                <h3>{data[1].title}.</h3>
+                                <p>{data[1].text}</p>
                         </div>
                         </div> 
                     </div>
                 </div>
             </div>
+            :
+            'Loading....'
+            }
         </div>
     )
 }
